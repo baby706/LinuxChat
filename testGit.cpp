494df357390d933abc580e71a3413f8652cca6c6
baby706 <cs-b@163.com>
2016-11-19 15:16:15 +0800

    this is a test

---
+++ testGit.cpp
@@ -0,0 +1,18 @@
+/*************************************************************************
+    > File Name: testGit.cpp
+    > File Path: /Users/liangbo/Downloads/Git/LinuxChat/testGit.cpp
+    > Author: Liang Bo
+    > Mail: cs-b@163.com 
+    > Funtion:
+    > Created Time: 
+ ************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	cout<<"hello world!"<<endl;
+	return 0;
+}
